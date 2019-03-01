6dd3405d9e2c03341499dc859dbe23e589430ca9
gaoshengyun <18616242909@163.com>
2019-03-01 20:22:03 +0800

    底部导航图标调整

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@
 				<span class="mui-tab-label">首页</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon icon-huiyuan"><span class="mui-badge">9</span></span>
+				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon icon-gouwuche"></span>
+				<span class="mui-icon icon-gouwuche"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-map">

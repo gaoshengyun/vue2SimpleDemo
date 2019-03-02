c8f34acf90077eb87794da4977db1dfa8da14ca3
gaoshengyun <18616242909@163.com>
2019-03-02 16:30:28 +0800

    组件动画

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app" class="app-container">
     <mt-header fixed title="固定在顶部"></mt-header>
-    <router-view/>
-
+    <transition>
+      <router-view/>
+    </transition>
     <nav class="mui-bar mui-bar-tab">
 			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="member">
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="chopcar">
+			<router-link class="mui-tab-item" to="/shopcar">
 				<span class="mui-icon icon-gouwuche"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="search">
+			<router-link class="mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">查找</span>
 			</router-link>
@@ -30,10 +31,24 @@ export default {
 }
 </script>
 
-<style scope>
+<style scoped>
 .app-container{
   padding-top: 40px;
   overflow-x: hidden;
   padding-bottom: 50px;
+}
+
+.v-enter,.v-leave-to{
+  opacity: 0;
+  transform: translateX(100%);
+  position: absolute;
+}
+.v-leave-to{
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+.v-enter-active,.v-leave-active{
+  transition: all .8s ease
 }
 </style>

2e7c36bba9937fb11df77cfd397dde6a107aa3fe
gaoshengyun <18616242909@163.com>
2019-03-02 14:55:07 +0800

    修改路由

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,22 @@
     <router-view/>
 
     <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
+			</router-link>
+			<router-link class="mui-tab-item" to="member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
+			</router-link>
+			<router-link class="mui-tab-item" to="chopcar">
 				<span class="mui-icon icon-gouwuche"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
+			</router-link>
+			<router-link class="mui-tab-item" to="search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">查找</span>
-			</a>
+			</router-link>
 		</nav>
   </div>
 </template>

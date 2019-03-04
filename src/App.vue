a85ce73b137e4e81b235b81efcdea0c4f4e81e75
gaoshengyun <18616242909@163.com>
2019-03-04 19:15:41 +0800

    图片分享路由

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,19 @@
       <router-view/>
     </transition>
     <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item" to="/home">
+			<router-link class="mui-tab-item-repair" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/member">
+			<router-link class="mui-tab-item-repair" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/shopcar">
+			<router-link class="mui-tab-item-repair" to="/shopcar">
 				<span class="mui-icon icon-gouwuche"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/search">
+			<router-link class="mui-tab-item-repair" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">查找</span>
 			</router-link>
@@ -50,5 +50,33 @@ export default {
 }
 .v-enter-active,.v-leave-active{
   transition: all .8s ease
+}
+
+.mui-bar-tab .mui-tab-item-repair.mui-active{
+  color: #007aff;
+}
+.mui-bar-tab .mui-tab-item-repair{
+  display: table-cell;
+  overflow: hidden;
+  width: 1%;
+  height: 50px;
+  text-align: center;
+  vertical-align: middle;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: #929292;
+}
+.mui-bar-tab .mui-tab-item-repair .mui-icon{
+  top: 3px;
+  width: 24px;
+  height: 24px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.mui-bar-tab .mui-tab-item-repair .mui-icon~.mui-tab-label{
+  font-size: 11px;
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>

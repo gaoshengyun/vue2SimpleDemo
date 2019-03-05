935c65514fbda7f70bf5abc587e456510a273888
gaoshengyun <18616242909@163.com>
2019-03-05 22:16:34 +0800

    缩略图功能

--- src/components/photos/PhotoInfo.vue
+++ src/components/photos/PhotoInfo.vue
@@ -6,6 +6,9 @@
       <span>点击次数:{{photoInfo.click}}</span>
     </p>
     <hr>
+    <div class="thumimg">
+      <vue-preview :slides="slide1" @close="handleClose"></vue-preview>
+    </div>
 
     <div class="content" v-html="photoInfo.content"></div>
 
@@ -21,20 +24,36 @@ export default {
   data() {
     return {
       id:this.$route.params.id,
-      photoInfo:{}
+      photoInfo:{},
+      slide1:[]
     }
   },
   created() {
     this.getPhotoInfo()
+    this.getthumimage()
   },
   methods: {
     getPhotoInfo() {
       this.axios.get('/api/getimageinfo/'+this.id).then((result) => {
         if (result.status === 200) {
           this.photoInfo = result.data.message[0]
-          console.log(result.data.message[0])
         }
       })
+    },
+    getthumimage(){
+      this.axios.get('/api/getthumimages/'+this.id).then((result) => {
+        if(result.status === 200){
+          result.data.message.forEach(item => {
+            item.w = 600
+            item.h = 400
+            item.msrc = item.src
+          });
+          this.slide1 = result.data.message
+        }
+      })
+    },
+    handleClose () {
+      console.log('close event')
     }
   },
   components:{
@@ -61,5 +80,17 @@ export default {
 .photoinfo-container .content{
   font-size: 13px;
   line-height: 30px;
+}
+.thumimg img{
+  margin: 10px;
+  box-shadow: 0 0 9px #999;
+  width: 100px;
+}
+.thumimg img.pswp__img{
+  margin: 0;
+}
+.thumimg figure{
+  margin: 0;
+  display: inline-block;
 }
 </style>

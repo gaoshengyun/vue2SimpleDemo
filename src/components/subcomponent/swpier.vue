c72dc5282aec96d578c70da858ed909a4ff5e74e
gaoshengyun <18616242909@163.com>
2019-03-07 16:51:32 +0800

    轮播图组件

---
+++ src/components/subcomponent/swpier.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="(item, index) in lunboList" :key="index">
+        <a :href="item.url">
+          <img :src="item.src" alt>
+        </a>
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+<script>
+export default {
+  props:['lunboList']
+}
+</script>
+<style>
+.mint-swipe {
+  height: 200px;
+}
+
+.mint-swipe-item img {
+  width: 100%;
+}
+</style>
+

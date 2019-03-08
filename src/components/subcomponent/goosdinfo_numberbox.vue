46e622e0448dba2388ba6ad9f8f5e260c478dcbd
gaoshengyun <18616242909@163.com>
2019-03-08 10:50:04 +0800

    商品详情的numberbox组件

---
+++ src/components/subcomponent/goosdinfo_numberbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="goodsinfo_numberbox">
+    <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
+      <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+      <input id="test" class="mui-input-numbox" type="number" value="1">
+      <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+  </div>
+</template>
+<script>
+import mui from '../../static/vendor/mui/dist/js/mui.min.js'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    mui('.mui-numbox').numbox()
+  },
+}
+</script>
+<style>
+.goodsinfo_numberbox{
+  display: inline-block;
+}
+</style>

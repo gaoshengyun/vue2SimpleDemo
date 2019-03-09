b51c1f1804e44a09c9a564be90cac4a134384fa3
gaoshengyun <18616242909@163.com>
2019-03-09 15:30:06 +0800

    商品评论

--- src/components/goods/GoodsComment.vue
+++ src/components/goods/GoodsComment.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    商品评论
+    <cmtbox></cmtbox>
   </div>
 </template>
 <script>
+import cmtbox from '../subcomponent/comment'
 export default {
-  
+  components:{
+    cmtbox
+  }
 }
 </script>
 <style>

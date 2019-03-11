31e65c662e6d3cbd48f282737c5c544dd5032646
gaoshengyun <18616242909@163.com>
2019-03-11 16:33:59 +0800

    购买商品数量子组件传值给父组件

--- src/components/subcomponent/goosdinfo_numberbox.vue
+++ src/components/subcomponent/goosdinfo_numberbox.vue
@@ -2,7 +2,7 @@
   <div class="goodsinfo_numberbox">
     <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
       <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-      <input id="test" class="mui-input-numbox" type="number" value="1">
+      <input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref="numbox">
       <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
     </div>
   </div>
@@ -16,7 +16,13 @@ export default {
     }
   },
   mounted() {
-    mui('.mui-numbox').numbox()
+    mui('.mui-numbox').numbox() //初始化
+  },
+  methods: {
+    countChanged(){
+      //console.log(this.$refs.numbox.value)
+      this.$emit('getcount',parseInt(this.$refs.numbox.value))
+    }
   },
 }
 </script>
